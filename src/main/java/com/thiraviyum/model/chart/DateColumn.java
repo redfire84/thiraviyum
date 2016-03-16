b41c1d55333e7f61884c13e9f6dd39205828158f
redfire84 <sekar.arasu@gmail.com>
2016-03-15 20:54:22 -0500

    Java8 LocalDate to DB column converter

--- src/main/java/com/thiraviyum/model/chart/DateColumn.java
+++ src/main/java/com/thiraviyum/model/chart/DateColumn.java
@@ -1,7 +1,6 @@
 package com.thiraviyum.model.chart;
 
 import java.time.LocalDate;
-import java.util.Calendar;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -14,11 +13,9 @@ public class DateColumn extends Column<LocalDate> {
 	@JsonProperty(value = "v")
 	public String getValue() {
 		StringBuffer sf = new StringBuffer("Date(");
-		Calendar cal = Calendar.getInstance();
-		cal.setTimeInMillis(getV().toEpochDay());
-		sf.append(cal.get(Calendar.YEAR)).append(",");
-		sf.append(cal.get(Calendar.MONTH)).append(",");
-		sf.append(cal.get(Calendar.DAY_OF_MONTH));
+		sf.append(getV().getYear()).append(",");
+		sf.append(getV().getMonthValue() - 1).append(",");
+		sf.append(getV().getDayOfMonth());
 		sf.append(")");
 		return sf.toString();
 	}
